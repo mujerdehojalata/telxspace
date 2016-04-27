577fb354ecdd58f0fdc0b4e7ee301532bf229414
Karen Marilú Valdivia Símiche <karenmvaldivia@gmail.com>
2016-04-27 21:16:27 +0000

    member.css y member.js commit karen

---
+++ js/member.js
@@ -0,0 +1,154 @@
+var chartData = generateChartData();
+
+function generateChartData() {
+  var chartData = [];
+  var firstDate = new Date( 2012, 0, 1 );
+  firstDate.setDate( firstDate.getDate() - 1000 );
+  firstDate.setHours( 0, 0, 0, 0 );
+
+  for ( var i = 0; i < 1000; i++ ) {
+    var newDate = new Date( firstDate );
+    newDate.setHours( 0, i, 0, 0 );
+
+    var a = Math.round( Math.random() * ( 40 + i ) ) + 100 + i;
+    var b = Math.round( Math.random() * 100000000 );
+
+    chartData.push( {
+      date: newDate,
+      value: a,
+      volume: b
+    } );
+  }
+  return chartData;
+}
+
+var chart = AmCharts.makeChart( "chartdiv", {
+  "type": "stock",
+  "theme": "dark",
+
+  "categoryAxesSettings": {
+    "minPeriod": "mm"
+  },
+
+  "dataSets": [ {
+    "color": "#b0de09",
+    "fieldMappings": [ {
+      "fromField": "value",
+      "toField": "value"
+    }, {
+      "fromField": "volume",
+      "toField": "volume"
+    } ],
+
+    "dataProvider": chartData,
+    "categoryField": "date"
+  } ],
+
+  "panels": [ {
+    "showCategoryAxis": false,
+    "title": "Value",
+    "percentHeight": 70,
+
+    "stockGraphs": [ {
+      "id": "g1",
+      "valueField": "value",
+      "type": "smoothedLine",
+      "lineThickness": 2,
+      "bullet": "round"
+    } ],
+
+
+    "stockLegend": {
+      "valueTextRegular": " ",
+      "markerType": "none"
+    }
+  }, {
+    "title": "Volume",
+    "percentHeight": 30,
+    "stockGraphs": [ {
+      "valueField": "volume",
+      "type": "column",
+      "cornerRadiusTop": 2,
+      "fillAlphas": 1
+    } ],
+
+    "stockLegend": {
+      "valueTextRegular": " ",
+      "markerType": "none"
+    }
+  } ],
+
+  "chartScrollbarSettings": {
+    "graph": "g1",
+    "usePeriod": "10mm",
+    "position": "top"
+  },
+
+  "chartCursorSettings": {
+    "valueBalloonsEnabled": true
+  },
+
+  "periodSelector": {
+    "position": "top",
+    "dateFormat": "YYYY-MM-DD JJ:NN",
+    "inputFieldWidth": 150,
+    "periods": [ {
+      "period": "hh",
+      "count": 1,
+      "label": "1 hour",
+      "selected": true
+    }, {
+      "period": "hh",
+      "count": 2,
+      "label": "2 hours"
+    }, {
+      "period": "hh",
+      "count": 5,
+      "label": "5 hour"
+    }, {
+      "period": "hh",
+      "count": 12,
+      "label": "12 hours"
+    }, {
+      "period": "MAX",
+      "label": "MAX"
+    } ]
+  },
+
+  "panelsSettings": {
+    "usePrefixes": true
+  },
+
+  "export": {
+    "enabled": true,
+    "position": "bottom-right"
+  }
+} );
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+
